3d3016e2c3739f8872b3a5802088d677c5cb3cda
rex_lee <lxailcquwer@qq.com>
2019-02-11 17:08:22 +0800

    base

--- ppmall-common/ppmall-common-base/src/main/java/com/yc/common/utils/RedisUtil.java
+++ ppmall-common/ppmall-common-base/src/main/java/com/yc/common/utils/RedisUtil.java
@@ -130,13 +130,13 @@ public class RedisUtil {
 	 * @param value
 	 *            值
 	 * @param time
-	 *            时间(秒) time要大于0 如果time小于等于0 将设置无限期
+	 *            时间(毫秒) time要大于0 如果time小于等于0 将设置无限期
 	 * @return true成功 false 失败
 	 */
 	public boolean set(String key, Object value, long time) {
 		try {
 			if (time > 0) {
-				redisTemplate.opsForValue().set(key, value, time, TimeUnit.SECONDS);
+				redisTemplate.opsForValue().set(key, value, time, TimeUnit.MILLISECONDS);
 			} else {
 				set(key, value);
 			}
